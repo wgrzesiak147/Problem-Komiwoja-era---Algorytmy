c4f58a6edbf525dde7b03af481ffcd37013c8e91
Wojciech Grzesiak <wgrzesiak147@gmail.com>
2015-11-13 22:07:51 +0100

    First merge

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Dialog w;
     w.show();
-
+    int i =10;
+    QString name = "siema tu Wojtek";
+    QString name2 = "TROLOLO";
     return a.exec();
 }
