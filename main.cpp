b3fb37d22b940d691a5f6e0445f6ab6ce202b167
Krzysztof Wawrzonkowski <k.wawrzonkowski@gmail.com>
2015-11-13 22:08:28 +0100

    test commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "dialog.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Dialog w;
     w.show();
+    qDebug() << "***Test code KW";
 
     return a.exec();
 }
